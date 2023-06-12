67b82026abd308bb9cf3633253e6a5a6c874715e
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-06-13 00:56:36 +0100

    Add test case with its own Makefile

---
+++ test/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include "list.h"
+
+struct IntNode {
+    struct ListNode node;
+    int data;
+};
+
+static int compare(struct ListNode *a, struct ListNode *b) {
+    struct IntNode *x = NODE_TO_DATA(a, struct IntNode, node);
+    struct IntNode *y = NODE_TO_DATA(b, struct IntNode, node);
+    return x - y;
+}
+
+static void print(struct ListNode *node) {
+    printf("%d", NODE_TO_DATA(node, struct IntNode, node)->data);
+}
+
+int main(void) {
+    struct List list;
+    init_list(&list, compare, print);
+    struct IntNode nodes[] = {
+        {.data = 1},
+        {.data = 2},
+        {.data = 3},
+        {.data = 4},
+        {.data = 5},
+    };
+    for (int i = 0; i < 5; ++i) {
+        insert_index(&list, i, &nodes[i].node);
+    }
+    print_list(&list);
+}
